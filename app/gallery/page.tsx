5e3684fbca90a9a08b46cf55bfef4af6d73fc65b
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-07-23 23:18:46 +0700

    Gallery page responsiveness

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -30,9 +30,9 @@ export const metadata: Metadata = {
 
 const col1 = [
   { imageName: 1, alt: "4th Class @ Cratloe Speechfest 2019!" },
-  { imageName: 2, alt: "The Boys!" },
+  { imageName: 2, alt: "The boys!" },
   { imageName: 3, alt: "Feile Luimnigh winners!" },
-  { imageName: 16, alt: "Annual Showcase Individual Performances!" },
+  { imageName: 16, alt: "Annual Showcase individual performances!" },
 ];
 
 const col2 = [
@@ -47,13 +47,13 @@ const col3 = [
   { imageName: 10, alt: "Senior Feile Luimnigh winners!" },
   { imageName: 11, alt: "Senior class photo!" },
   { imageName: 12, alt: "Class projects!" },
-  { imageName: 17, alt: "Lots of wins at Feile Luimnigh" },
+  { imageName: 17, alt: "Lots of wins at Feile Luimnigh!" },
 ];
 
 const col4 = [
-  { imageName: 13, alt: "Annual Showcase Awards Presentation!" },
-  { imageName: 14, alt: "Exams Medalist from the Irish Board!" },
-  { imageName: 15, alt: "Annual Showcase Awards Presentation!" },
+  { imageName: 13, alt: "Annual Showcase awards presentation!" },
+  { imageName: 14, alt: "Exam results from the Irish Board!" },
+  { imageName: 15, alt: "Annual Showcase awards presentation!" },
   { imageName: 4, alt: "Senior class photo!" },
 ];
 
@@ -65,25 +65,25 @@ const Gallery = () => {
       <h1 className="text-center text-2xl md:text-4xl mt-10 mb-2" id="benefits">
         Our Gallery
       </h1>
-      <p className="text-center text-base mb-10 md:w-1/2 mx-auto">
+      <p className="text-center text-base mb-10 sm:w-4/5 lg:w-1/2 mx-auto">
         We take pride in our students' achievements and the fun and memories
         they make at our school during classtime. Here is a taste of all the
         smiles we've seen over the years. More photos and updates over on our
         Instagram and Facebook communities!
       </p>
-      <div className="flex flex-row space-x-5 pb-10">
-        {cols.map((col) => {
+      <div className="mb-10 grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-4 md:grid-cols-4">
+        {cols.map((col, i) => {
           return (
-            <div className="w-1/4 flex flex-col space-y-5">
+            <div key={`photo_col_${i}`} className="grid grid-row-2 gap-y-4">
               {col.map((img, i) => {
                 return (
-                  <div className="group relative">
+                  <div className="group relative flex flex-col justify-between">
                     <img
-                      key={`${img.imageName}_$i`}
+                      key={`${img.imageName}_${i}`}
                       src={`/gallery/${img.imageName}.jpg`}
-                      className="object-contain group"
+                      className="object-cover w-full group brightness-105 hue-rotate-15 h-full"
                     />
-                    <p className="group absolute w-full bottom-0 h-0 flex-col text-gray-200 text-center justify-center transition-all bg-violet-500 group-hover:h-full  group-hover:transition-all group-hover:flex  group-hover:p-5  opacity-0 group-hover:opacity-100">
+                    <p className="group p-5 min-h-10 flex md:absolute w-full bottom-0 md:h-0 flex-col text-gray-200 text-center justify-center transition-all bg-violet-500 group-hover:h-full group-hover:transition-all group-hover:flex group-hover:p-5 md:opacity-0 group-hover:opacity-100">
                       {img.alt}
                     </p>
                   </div>
