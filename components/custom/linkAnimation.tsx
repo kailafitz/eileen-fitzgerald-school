b5f085b7e78c045f4fdf01f63ee696e19fbb6bac
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-08-05 00:49:13 +0700

    Underline spacing fix

--- components/custom/linkAnimation.tsx
+++ components/custom/linkAnimation.tsx
@@ -11,11 +11,11 @@ const LinkAnimation = (props: Props) => {
     <span
       className={`${
         props.underline
-          ? "before:w-full before:h-0.5 before:absolute before:block before:-bottom-1 before:left-0 before:bg-gray-200 before:scale-x-100"
+          ? "before:w-full before:h-0.5 before:absolute before:block before:-bottom-0.5 before:left-0 before:bg-gray-200 before:scale-x-100"
           : ""
       } after:w-full after:h-0.5 after:absolute after:block after:${
         props.mb ? "-bottom-2" : "-bottom-0.5"
-      } after:left-0 after:bg-primary after:scale-x-0 after:transition-all hover:after:scale-x-100`}
+      } after:left-0 after:bg-primary after:scale-x-0 after:transition-all hover:after:scale-x-100 z-20`}
     >
       {props.children}
     </span>
