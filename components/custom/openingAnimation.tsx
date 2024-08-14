2f96ffa1966c962a13b1d8a8216d9a07fafa9e67
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-08-14 19:53:24 +1000

    Animation only runs once on homepage

--- components/custom/openingAnimation.tsx
+++ components/custom/openingAnimation.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 const OpeningAnimation = () => {
+  const [animationActivate, setAnimationActivate] = useState(true);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimationActivate(false);
+    }, 7000);
+  }, []);
+
   return (
-    <div className="bg-zinc-900 z-50 fixed top-0 left-0 h-screen w-screen flex flex-col justify-center animate-slide-out-container">
-      <div className="bg-white absolute rounded-full animate-spotlight"></div>
-      <img
-        src="/logo.png"
-        className="w-52 h-52 mx-auto animate-logo"
-        alt="logo"
-      />
-      <h1 className="font-heading animate-logo text-center text-2xl w-full absolute top-[65%]">
-        The Eileen Fitzgerald <br /> School of Speech and Drama
-      </h1>
-    </div>
+    <>
+      {pathname === "/" && animationActivate && (
+        <div className="bg-zinc-900 z-50 fixed top-0 left-0 h-screen w-screen flex flex-col justify-center animate-slide-out-container">
+          <div className="bg-white absolute rounded-full animate-spotlight"></div>
+          <img
+            src="/logo.png"
+            className="w-52 h-52 mx-auto animate-logo"
+            alt="logo"
+          />
+          <h1 className="font-heading animate-logo text-center text-2xl w-full absolute top-[65%]">
+            The Eileen Fitzgerald <br /> School of Speech and Drama
+          </h1>
+        </div>
+      )}
+    </>
   );
 };
 
